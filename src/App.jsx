4070367bd4a604ad22da7cad74c49effb06a88bb
shabas123 <shazz4568@gmail.com>
2025-06-19 10:33:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 import Header from './Components/Header';
@@ -8,35 +8,37 @@ import Register from './Pages/Register';
 import Adminscreen from './Components/Adminscreen';
 import Order from './Components/Order';
 import { useEffect, useState } from 'react';
-import CartModel from './Components/CartModel';
+import Entry from './Pages/Entry';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const location = useLocation();
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
 
-    const [user, setUser] = useState(null);
-  
-    useEffect(() => {
-      const storedUser = localStorage.getItem('user');
-      if (storedUser) {
-        setUser(JSON.parse(storedUser));
-      }
-    }, []);
+ 
+  const isEntryPage = location.pathname === "/";
+
   return (
     <>
-      <Header user={user}/>
+     
+      {!isEntryPage && <Header user={user} />}
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route  path="/cart" element={<Cart />} />
-        <Route  path="/cartmodal" element={<Cart />} />
+        <Route path="/" element={<Entry />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/cartmodal" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin/*" element={<Adminscreen />} />
         <Route path="/order" element={<Order />} />
       </Routes>
-
-      
     </>
-
-    
   );
 }
 
