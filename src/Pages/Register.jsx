71eecf4b0b99ec1429237442c217b369ecddc4dd
shabas123 <shazz4568@gmail.com>
2025-06-19 09:03:59 +0530

    Update Register.jsx

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -64,7 +64,6 @@ export default function Register({ register }) {
           <div className="row mt-4">
             <div className="col-md-4"></div>
             <div className="col-md-6 border-white rounded  mt-4">
-              <h3 className='text-black text-center'>Register</h3>
 
             </div>
 
