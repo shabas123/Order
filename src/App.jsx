671202a140938eb918f3817aa228b7b2507c7d87
shabas123 <shazz4568@gmail.com>
2025-06-20 10:32:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,25 @@ import { useEffect, useState } from 'react';
 import Entry from './Pages/Entry';
 
 function App() {
+  const [user, setUser] = useState(null);
   const location = useLocation();
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+ 
   const isEntryPage = location.pathname === "/";
 
   return (
     <>
      
-      {!isEntryPage && <Header/>}
+      {!isEntryPage && <Header user={user} />}
 
       <Routes>
-        <Route path="entry" element={<Entry />} />
+        <Route path="/entry" element={<Entry />} />
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/cartmodal" element={<Cart />} />
