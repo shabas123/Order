c7bd14e69063a01a183fb5d2d0687ca01418a942
shabas123 <shazz4568@gmail.com>
2025-12-18 21:08:38 +0530

    Refactor Home component for better readability

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -31,13 +31,14 @@ export default function Home() {
 
      <div className="container-fluid d-flex align-items-center justify-content-center bg">
         <div className="row align-items-center" style={{textAlign:"center"}}>
-          <div className="col-md-6 " style={{ padding: "30px" }}>
-        {  loggedInUser ?  <h2 className="fw-bold text-success " > Welcome {loggedInUser} </h2>
-        :
-       <Link to={"/login"} style={{textDecoration:"none", fontFamily:"fantasy"}} ><h2>!!  Touch Here !!  </h2></Link>
-        
-      }
-           {loggedInUser && <div><h1 className="fw-bold text-danger">Delicious food <br />always <span style={{ fontFamily: 'fantasy' }}>tells a story</span> </h1>
+                    <div className="col-md-6 " style={{ padding: "30px" }}>
+            {loggedInUser ? <h2 className="fw-bold text-success " > Welcome {loggedInUser} </h2>
+              :
+              <Link to={"/"} style={{ textDecoration: "none", fontFamily: "fantasy" }} ><h2></h2></Link>
+              <Link to={"/entry"} style={{ textDecoration: "none", fontFamily: "fantasy" }} ><h2></h2></Link>
+
+            }
+            <div><h1 className="fw-bold text-danger">Delicious food <br />always <span style={{ fontFamily: 'fantasy' }}>tells a story</span> </h1>
              <h4 className="mt-3  fw-bold" style={{ color: "red" , textAlign:"center" }}>
                Welcome to our restaurant, where every dish is crafted with passion and precision. Enjoy a culinary adventure filled with mouth-watering flavors and unforgettable moments.
             </h4>
@@ -118,3 +119,4 @@ export default function Home() {
     </>
   );
     }
+
